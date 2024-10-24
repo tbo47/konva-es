945fedc041d23ab22038f2ef28969e494ff52630
Thibaut(Teebo) <thibaut@todosbuild.com>
2024-10-24 09:27:29 +0200

    konva-es.9.3.16

--- build-esm-bundle.sh
+++ build-esm-bundle.sh
@@ -1,8 +1,8 @@
 set -e
 rm -rf dist
-npx esbuild konva/src/index.ts --bundle --platform=neutral --packages=external --outfile=dist/konva-es.9.3.15.js --minify-syntax
+npx esbuild ../konva/src/index.ts --bundle --platform=neutral --packages=external --outfile=dist/konva-es.9.3.16.js --minify-syntax
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's/src_default as default/src_default as Konva/g' dist/konva-es.9.3.15.js
+  sed -i '' 's/src_default as default/src_default as Konva/g' dist/konva-es.9.3.16.js
 else
-  sed -i 's/src_default as default/src_default as Konva/g' dist/konva-es.9.3.15.js
+  sed -i 's/src_default as default/src_default as Konva/g' dist/konva-es.9.3.16.js
 fi
