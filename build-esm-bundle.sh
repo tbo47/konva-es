8f3d4a3ecf5f2cfce9e365bd74f51e81dadab634
tbo47 <thibaut@todosbuild.com>
2024-12-24 05:38:58 +0000

    konva 9.3.18

--- build-esm-bundle.sh
+++ build-esm-bundle.sh
@@ -1,8 +1,9 @@
 set -e
 rm -rf dist
-npx esbuild ../konva/src/index.ts --bundle --platform=neutral --packages=external --outfile=dist/konva-es.9.3.16.js --minify-syntax
+npx esbuild ../konva/src/index.ts --bundle --platform=neutral --packages=external --outfile=dist/konva-es.9.3.18.js --minify-syntax
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's/src_default as default/src_default as Konva/g' dist/konva-es.9.3.16.js
+  sed -i '' 's/index_default as default/index_default as Konva/g' dist/konva-es.9.3.18.js
 else
-  sed -i 's/src_default as default/src_default as Konva/g' dist/konva-es.9.3.16.js
+  sed -i 's/index_default as default/index_default as Konva/g' dist/konva-es.9.3.18.js
 fi
+cp dist/konva-es.9.3.18.js ../tbo47.github.io/
--- build-esm.sh
+++ build-esm.sh
@@ -5,6 +5,7 @@ npm install
 cd ../konva
 git checkout .
 rm src/index-node.ts 
+# it's the sed syntax for macos, it can easily be changed for linux
 sed -i '' '30,31d' src/Global.ts
 sed -i '' '30s/://' src/Global.ts
 # it's strange, sometimes it's not usefull to do the following
