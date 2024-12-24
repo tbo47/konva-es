2e8274e65aef9ece46dc52afb7d5d22f378ca804
tbo47 <thibaut@todosbuild.com>
2024-12-24 05:59:29 +0000

    9.3.19

--- build-esm-bundle.sh
+++ build-esm-bundle.sh
@@ -1,9 +1,15 @@
 set -e
 rm -rf dist
-npx esbuild ../konva/src/index.ts --bundle --platform=neutral --packages=external --outfile=dist/konva-es.9.3.18.js --minify-syntax
+
+# Read version number from package.json
+VERSION=$(jq -r '.version' package.json)
+
+echo "Building ESM bundle for Konva version $VERSION"
+
+npx esbuild ../konva/src/index.ts --bundle --platform=neutral --packages=external --outfile=dist/konva-es.$VERSION.js --minify-syntax
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's/index_default as default/index_default as Konva/g' dist/konva-es.9.3.18.js
+  sed -i '' 's/index_default as default/index_default as Konva/g' dist/konva-es.$VERSION.js
 else
-  sed -i 's/index_default as default/index_default as Konva/g' dist/konva-es.9.3.18.js
+  sed -i 's/index_default as default/index_default as Konva/g' dist/konva-es.$VERSION.js
 fi
-cp dist/konva-es.9.3.18.js ../tbo47.github.io/
+cp dist/konva-es.$VERSION.js ../tbo47.github.io/
