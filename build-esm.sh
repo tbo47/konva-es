5dbe10e14b978e0e2bc8f69c86304aa5d514f4e3
tbo47 <thibaut.lassalle@gmail.com>
2025-10-07 10:27:17 +0000

    10.0.2

--- build-esm.sh
+++ build-esm.sh
@@ -5,14 +5,12 @@ mkdir dist
 npm install
 cd ../konva
 git checkout .
-rm src/index-node.ts 
 # it's the sed syntax for macos, it can easily be changed for linux
 sed -i '' '30,31d' src/Global.ts
 sed -i '' '30s/://' src/Global.ts
 # it's strange, sometimes it's not usefull to do the following
 # https://github.com/konvajs/konva/pull/1828/files
 sed -i '' 's/\]\.isDragging/\]\?.isDragging/g' src/Global.ts
-sed -i '' 's/CommonJS/ESNext/g' tsconfig.json
 tsc --removeComments
 git checkout .
 cd ../konva-es
